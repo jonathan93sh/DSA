5e3f64c817f2f3a8533fe8796d3eff178763d99a
jonathan93sh <schouga4rd@gmail.com>
2016-02-17 23:13:59 +0100

    v0.2

--- Case1/FSKanalyser.m
+++ Case1/FSKanalyser.m
@@ -1,16 +1,18 @@
 %function [ besked ] = FSKanalyser( x, fs, Baudrate )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
-
+clear
 fstart = 500;
 fstop = 10000;
 fast=1;
-
+Baudrate=2;
 fstep=(fstop-fstart)/256
 
 load('record.mat')
 
 x=x';
+
+sound(x, fs)
 
 N_min=round(fs/fstep);%antal samples der skal være før frekvens opløsningen er stor nok.
 
@@ -31,7 +33,7 @@ if(Ncut<N_min)
 end
 
 if(fast)
-    Spektro=zeros(cut, N_min);
+    Spektro=zeros(cut, round(N_min/2));
 else
     Spektro=zeros(cut, round(Ncut/2));
 end
@@ -43,14 +45,20 @@ for n = [1:cut]
     %Spektro(n,:)=
     disp(n);
     if(fast)
-        Spektro(n,:)=mDFT(x(Ncut*(n-1)+1:(Ncut*(n-1))+N_min*n));
+        Spektro(n,:)=mDFT(hamming(N_min).*x(Ncut*(n-1)+1:(Ncut*(n-1))+N_min));
     else
-        Spektro(n,:)=mDFT(x(Ncut*(n-1)+1:Ncut*n));
+        Spektro(n,:)=mDFT(hamming(Ncut).*x(Ncut*(n-1)+1:Ncut*n));
     end
-    test = 
 end
 
+frekvenser=zeros(cut, 3);
 
 
+for n = [1:cut]
+    if(fast)
+        [frekvenser(n,1), frekvenser(n,2)]=max(Spektro(n,:));
+        frekvenser(n,3)=10*log10(max(Spektro(n,:).^2)/(sum(Spektro(n,:).^2)-max(Spektro(n,:).^2)))
+    end
+end
 %end
 
