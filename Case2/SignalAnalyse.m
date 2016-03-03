db3ff7f77c53542b16d21eb8c4ce6f2cdc97692b
RedInsanity <laimutis.bendikas@gmail.com>
2016-03-03 14:44:47 +0100

    opg 1 almost done

--- Case2/SignalAnalyse.m
+++ Case2/SignalAnalyse.m
@@ -5,8 +5,14 @@ close all
 
 [x,fs] = audioread('tale_tone_48000.wav');
 
-plot(abs(fft(x)))
-%fft_plot(X,fs,'Analyse af signal',10,10);
+plot(x)
+set(gcf,'PaperUnits','inches','PaperPosition',[0 0 10 10])
+print('foto/tidssignal FØR filtrering opg1' ,'-dpng')
 
+fft_plot(fft(x),fs,'Analyse af tone',10,10);
+%ud fra billederne kan man danne sig en idé om at tonen ligger ved 785Hz
 
-spectrogram(x,hamming(1024),0,length(x),fs);
+figure
+spectrogram(x,hamming(4000),0,4000,fs);
+set(gcf,'PaperUnits','inches','PaperPosition',[0 0 20 10])
+print('foto/spektrogram analyse opg1' ,'-dpng')
--- Case2/fft_plot.m
+++ Case2/fft_plot.m
@@ -5,26 +5,15 @@ function [ ] = fft_plot( fft, fs, name, plot_x, plot_y )
 N=round(length(fft)/2);
 ts=fs/(length(fft));
 
-
-subplot(2,1,1);
+figure
 semilogx([0:N-1]*ts, 20*log10(abs(fft(1:N))));
 grid on
 xlabel('Hz')
 ylabel('dB')
 title(['amplitude (dB) - ' name])
 
-subplot(2,1,2);
-semilogx([0:N-1]*ts, unwrap(angle(fft(1:N)))*(180/pi));
-grid on
-xlabel('Hz')
-ylabel('grader')
-title(['fase forskydning'])
-
 set(gcf,'PaperUnits','inches','PaperPosition',[0 0 plot_x plot_y])
 print(['foto/fasespektrum ' name],'-dpng')
-
-
-
 
 end
 
